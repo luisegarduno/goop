3d85c2fa950eda906381a6591ba904e215aea1b3
Luis <luis.e.garduno@gmail.com>
2025-11-25 22:10:19 -0500

    Revert "chore(drizzle): remove 'biome-ignore' comment"
    This reverts commit 869eb77e8912fbcdb1c58a5a325c0f04822fa903.

--- packages/backend/drizzle.config.ts
+++ packages/backend/drizzle.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
+    // biome-ignore lint: Forbidden non-null assertion.
     url:
       process.env.DATABASE_URL || "postgresql://goop:pass123@localhost:5432/db",
   },
